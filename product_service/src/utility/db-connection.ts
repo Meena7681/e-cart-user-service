b6f11dbab4a02aaa6dc56bd71213848d9d9808db
Meena7681 <manishmeena7681@gmail.com>
2023-12-21 00:04:40 +0530

    connection

--- product_service/src/utility/db-connection.ts
+++ product_service/src/utility/db-connection.ts
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
 const ConnectDB = async () => {
-  const DB_URL =
-    "mongodb+srv://manishmeena7178:manishmeena@cluster0.9vehnn6.mongodb.net/nodejs-sls-mc";
+  const DB_URL = process.env.MONGODB_URL;
   try {
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(`"${DB_URL}"`);
   } catch (err) {
     console.log(err);
   }
